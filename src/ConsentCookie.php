951d002a3196478a3bdce66b51126603aabde76d
Geoffray <gv@vuble.tv>
2018-11-19 19:47:29 +0100

    Del: methode str2bin

--- src/ConsentCookie.php
+++ src/ConsentCookie.php
@@ -50,15 +50,4 @@ class ConsentCookie {
         }
     }
 
-    private function str2bin($str) {
-        $out = false;
-        for($a=0; $a < strlen($str); $a++)
-        {
-                $dec = ord(substr($str,$a,1)); //determine symbol ASCII-code
-                $bin = sprintf('%08d', base_convert($dec, 10, 2)); //convert to binary representation and add leading zeros
-                $out .= $bin;
-        }
-        return $out;
-    }
-
 }
