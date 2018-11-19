c55bf09376d8b42db2fc6fca4887bc3fa612f742
Geoffray <gv@vuble.tv>
2018-11-19 19:50:44 +0100

    Mod: check if str2bin exists

--- src/functions.php
+++ src/functions.php
@@ -1,12 +1,13 @@
 <?php
 
-function str2bin($str) {
-    $out = false;
-    for($a=0; $a < strlen($str); $a++)
-    {
-            $dec = ord(substr($str,$a,1)); //determine symbol ASCII-code
+if (!function_exists('spl_object_id')) {
+    function str2bin($str) {
+        $out = false;
+        for ($a = 0; $a < strlen($str); $a++) {
+            $dec = ord(substr($str, $a, 1)); //determine symbol ASCII-code
             $bin = sprintf('%08d', base_convert($dec, 10, 2)); //convert to binary representation and add leading zeros
             $out .= $bin;
+        }
+        return $out;
     }
-    return $out;
 }
