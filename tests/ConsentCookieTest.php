9a7586e5dd76b8d74149cc4a09a8561ceb467b3f
Geoffray <gv@vuble.tv>
2018-11-20 17:59:23 +0100

    Add: tests and add array_values to remvoe old keys on get vendors

--- tests/ConsentCookieTest.php
+++ tests/ConsentCookieTest.php
@@ -42,4 +42,89 @@ class ConsentCookieTest extends TestCase
 
         $this->isEmpty($consentCookie->getBitField());
     }
+
+    public function testRangeEntriesValuesConsentCookie()
+    {
+        $consentCookie = new ConsentCookie("BOXhscYOXhscYACABDENAE4AAAAAyADAALAAcACgAGA");
+
+        $this->assertEquals(0, $consentCookie->getDefaultConsent(), "DefaultConsent value not valid");
+        $this->assertEquals(3, $consentCookie->getNumEntries(), "NumEntries value not valid");
+
+        $this->assertEquals(
+            [
+                [
+                    "singleOrRange"     => 0,
+                    "singleVendorId"    => 5,
+                ],
+                [
+                    "singleOrRange"     => 1,
+                    "startVendorId"     => 7,
+                    "endVendorId"       => 10,
+                ],
+                [
+                    "singleOrRange"     => 0,
+                    "singleVendorId"    => 12,
+                ]
+            ],
+            $consentCookie->getRangeEntries(),
+            "NumEntries value not valid"
+        );
+
+        $consentCookie_2 = new ConsentCookie("BOXhscYOXhscYACABDENAE4AAAAAzADAALAAcACgAGA");
+
+        $this->assertEquals(1, $consentCookie_2->getDefaultConsent(), "DefaultConsent value not valid");
+    }
+
+    public function testRangeEntriesToArrayConsentCookie()
+    {
+        $consentCookie = new ConsentCookie("BOXhscYOXhscYACABDENAE4AAAAAyADAALAAcACgAGA");
+
+        $this->assertEquals(
+            [
+                "version"           => 1,
+                "created"           => "2018-11-20 10:23:49.600000",
+                "lastUpdated"       => "2018-11-20 10:23:49.600000",
+                "cmpId"             => 2,
+                "cmpVersion"        => 1,
+                "consentScreen"     => 3,
+                "consentLanguage"   => "EN",
+                "vendorListVersion" => 4,
+                "purposesAllowed"   => [
+                    1, 2, 3
+                ],
+                "maxVendorId"       => 12,
+                "encodingType"      => 1,
+                "vendorsAllowed"    => [
+                    5, 7, 8, 9, 10, 12
+                ],
+            ],
+            $consentCookie->toArray(),
+            "toArray return not valid"
+        );
+
+        $consentCookie_2 = new ConsentCookie("BOXhscYOXhscYACABDENAE4AAAAAzADAALAAcACgAGA");
+
+        $this->assertEquals(
+            [
+                "version"           => 1,
+                "created"           => "2018-11-20 10:23:49.600000",
+                "lastUpdated"       => "2018-11-20 10:23:49.600000",
+                "cmpId"             => 2,
+                "cmpVersion"        => 1,
+                "consentScreen"     => 3,
+                "consentLanguage"   => "EN",
+                "vendorListVersion" => 4,
+                "purposesAllowed"   => [
+                    1, 2, 3
+                ],
+                "maxVendorId"       => 12,
+                "encodingType"      => 1,
+                "vendorsAllowed"    => [
+                    1, 2, 3, 4, 6, 11
+                ],
+            ],
+            $consentCookie_2->toArray(),
+            "toArray return not valid"
+        );
+    }
 }
