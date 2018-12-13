75f75347ab952933b07eb11f28dd57284ba15721
allan <mifefr@gmail.com>
2018-12-14 00:23:32 +0100

    Mod: Code clean and more powerfull

--- tests/ConsentCookieTest.php
+++ tests/ConsentCookieTest.php
@@ -17,30 +17,29 @@ class ConsentCookieTest extends TestCase
     {
         $this->assertInstanceOf(
             ConsentCookie::class,
-            new ConsentCookie("BOXhscYOXhscYACABDENAE4AAAAAwQgA")
+            new ConsentCookie('BOXhscYOXhscYACABDENAE4AAAAAwQgA')
         );
     }
 
     public function test_values_from_string()
     {
-        $consentCookie = new ConsentCookie("BOXhscYOXhscYACABDENAE4AAAAAwQgA");
+        $consentCookie = new ConsentCookie('BOXhscYOXhscYACABDENAE4AAAAAwQgA');
 
-        $this->assertEquals(1, $consentCookie->getVersion(), "Version value not valid");
-        $this->assertEquals("2018-11-20 10:23:49.600000", $consentCookie->getCreated(), "Created value not valid");
-        $this->assertEquals("2018-11-20 10:23:49.600000", $consentCookie->getLastUpdated(), "LastUpdated value not valid");
-        $this->assertEquals(2, $consentCookie->getCmpId(), " CmpId value not valid");
-        $this->assertEquals(1, $consentCookie->getCmpVersion(), "CmpVersion value not valid");
-        $this->assertEquals(3, $consentCookie->getConsentScreen(), "ConsentScreen value not valid");
-        $this->assertEquals("EN", $consentCookie->getConsentLanguage(), "ConsentLanguage value not valid");
-        $this->assertEquals(4, $consentCookie->getVendorListVersion(), "VendorListVersion value not valid");
-        $this->assertEquals([1, 2, 3], $consentCookie->getPurposesAllowed(), "PurposesAllowed value not valid");
-        $this->assertEquals(12, $consentCookie->getMaxVendorId(), "MaxVendorId value not valid");
-        $this->assertEquals("0", $consentCookie->getEncodingType(), "EncodingType value not valid");
+        $this->assertEquals(1, $consentCookie->getVersion(), 'Version value not valid');
+        $this->assertEquals('2018-11-20 10:23:49.600000', $consentCookie->getCreated(), 'Created value not valid');
+        $this->assertEquals('2018-11-20 10:23:49.600000', $consentCookie->getLastUpdated(), 'LastUpdated value not valid');
+        $this->assertEquals(2, $consentCookie->getCmpId(), 'CmpId value not valid');
+        $this->assertEquals(1, $consentCookie->getCmpVersion(), 'CmpVersion value not valid');
+        $this->assertEquals(3, $consentCookie->getConsentScreen(), 'ConsentScreen value not valid');
+        $this->assertEquals('EN', $consentCookie->getConsentLanguage(), 'ConsentLanguage value not valid');
+        $this->assertEquals(4, $consentCookie->getVendorListVersion(), 'VendorListVersion value not valid');
+        $this->assertEquals([1, 2, 3], $consentCookie->getPurposesAllowed(), 'PurposesAllowed value not valid');
+        $this->assertEquals(12, $consentCookie->getMaxVendorId(), 'MaxVendorId value not valid');
+        $this->assertEquals('0', $consentCookie->getEncodingType(), 'EncodingType value not valid');
+        $this->assertEquals('001000010000', $consentCookie->getBitField(), 'BitField value not valid');
+        $this->assertEquals([3, 8], $consentCookie->getVendorsAllowed(), 'VendorsAllowed value not valid');
 
-        $this->assertEquals("001000010000", $consentCookie->getBitField(), "BitField value not valid");
-        $this->assertEquals([3, 8], $consentCookie->getVendorsAllowed(), "VendorsAllowed value not valid");
-
-        $this->isEmpty($consentCookie->getBitField());
+        $this->assertEquals(NULL, $consentCookie->getRangeEntries(), 'bitField should be null');
     }
 
     public function test_to_string_from_string()
@@ -63,123 +62,123 @@ class ConsentCookieTest extends TestCase
 
     public function test_range_entries_values()
     {
-        $consentCookie = new ConsentCookie("BOXhscYOXhscYACABDENAE4AAAAAyADAALAAcACgAGA");
+        $consentCookie = new ConsentCookie('BOXhscYOXhscYACABDENAE4AAAAAyADAALAAcACgAGA');
 
-        $this->assertEquals(0, $consentCookie->getDefaultConsent(), "DefaultConsent value not valid");
-        $this->assertEquals(3, $consentCookie->getNumEntries(), "NumEntries value not valid");
+        $this->assertEquals(0, $consentCookie->getDefaultConsent(), 'DefaultConsent value not valid');
+        $this->assertEquals(3, $consentCookie->getNumEntries(), 'NumEntries value not valid');
 
         $this->assertEquals(
             [
                 [
-                    "singleOrRange"     => 0,
-                    "singleVendorId"    => 5,
+                    'singleOrRange'     => 0,
+                    'singleVendorId'    => 5,
                 ],
                 [
-                    "singleOrRange"     => 1,
-                    "startVendorId"     => 7,
-                    "endVendorId"       => 10,
+                    'singleOrRange'     => 1,
+                    'startVendorId'     => 7,
+                    'endVendorId'       => 10,
                 ],
                 [
-                    "singleOrRange"     => 0,
-                    "singleVendorId"    => 12,
+                    'singleOrRange'     => 0,
+                    'singleVendorId'    => 12,
                 ]
             ],
             $consentCookie->getRangeEntries(),
-            "NumEntries value not valid"
+            'NumEntries value not valid'
         );
 
-        $consentCookie_2 = new ConsentCookie("BOXhscYOXhscYACABDENAE4AAAAAzADAALAAcACgAGA");
+        $consentCookie_2 = new ConsentCookie('BOXhscYOXhscYACABDENAE4AAAAAzADAALAAcACgAGA');
 
-        $this->assertEquals(1, $consentCookie_2->getDefaultConsent(), "DefaultConsent value not valid");
+        $this->assertEquals(1, $consentCookie_2->getDefaultConsent(), 'DefaultConsent value not valid');
     }
 
     public function test_range_entries_toArray()
     {
-        $consentCookie = new ConsentCookie("BOXhscYOXhscYACABDENAE4AAAAAyADAALAAcACgAGA");
+        $consentCookie = new ConsentCookie('BOXhscYOXhscYACABDENAE4AAAAAyADAALAAcACgAGA');
 
         $this->assertEquals(
             [
-                "version"           => 1,
-                "created"           => "2018-11-20 10:23:49.600000",
-                "lastUpdated"       => "2018-11-20 10:23:49.600000",
-                "cmpId"             => 2,
-                "cmpVersion"        => 1,
-                "consentScreen"     => 3,
-                "consentLanguage"   => "EN",
-                "vendorListVersion" => 4,
-                "purposesAllowed"   => [
+                'version'           => 1,
+                'created'           => '2018-11-20 10:23:49.600000',
+                'lastUpdated'       => '2018-11-20 10:23:49.600000',
+                'cmpId'             => 2,
+                'cmpVersion'        => 1,
+                'consentScreen'     => 3,
+                'consentLanguage'   => 'EN',
+                'vendorListVersion' => 4,
+                'purposesAllowed'   => [
                     1, 2, 3
                 ],
-                "maxVendorId"       => 12,
-                "encodingType"      => 1,
+                'maxVendorId'       => 12,
+                'encodingType'      => 1,
             ],
             $consentCookie->toArray(),
-            "toArray return not valid"
+            'toArray return not valid'
         );
 
-        $consentCookie_2 = new ConsentCookie("BOXhscYOXhscYACABDENAE4AAAAAzADAALAAcACgAGA");
+        $consentCookie_2 = new ConsentCookie('BOXhscYOXhscYACABDENAE4AAAAAzADAALAAcACgAGA');
 
         $this->assertEquals(
             [
-                "version"           => 1,
-                "created"           => "2018-11-20 10:23:49.600000",
-                "lastUpdated"       => "2018-11-20 10:23:49.600000",
-                "cmpId"             => 2,
-                "cmpVersion"        => 1,
-                "consentScreen"     => 3,
-                "consentLanguage"   => "EN",
-                "vendorListVersion" => 4,
-                "purposesAllowed"   => [
+                'version'           => 1,
+                'created'           => '2018-11-20 10:23:49.600000',
+                'lastUpdated'       => '2018-11-20 10:23:49.600000',
+                'cmpId'             => 2,
+                'cmpVersion'        => 1,
+                'consentScreen'     => 3,
+                'consentLanguage'   => 'EN',
+                'vendorListVersion' => 4,
+                'purposesAllowed'   => [
                     1, 2, 3
                 ],
-                "maxVendorId"       => 12,
-                "encodingType"      => 1,
+                'maxVendorId'       => 12,
+                'encodingType'      => 1,
             ],
             $consentCookie_2->toArray(),
-            "toArray return not valid"
+            'toArray return not valid'
         );
     }
 
     public function test_arePurposesAllowed()
     {
-        $consentCookie = new ConsentCookie("BOXhscYOXhscYACABDENAE4AAAAAyADAALAAcACgAGA");
+        $consentCookie = new ConsentCookie('BOXhscYOXhscYACABDENAE4AAAAAyADAALAAcACgAGA');
 
-        $this->assertEquals(false, $consentCookie->arePurposesAllowed([1, 2, 3, 4]), "Purposes should not be allowed");
-        $this->assertEquals(false, $consentCookie->arePurposesAllowed([4, 1, 2, 3]), "Purposes should not be allowed");
-        $this->assertEquals(false, $consentCookie->arePurposesAllowed([]), "Purposes should not be allowed");
+        $this->assertEquals(false, $consentCookie->arePurposesAllowed([1, 2, 3, 4]), 'Purposes should not be allowed');
+        $this->assertEquals(false, $consentCookie->arePurposesAllowed([4, 1, 2, 3]), 'Purposes should not be allowed');
+        $this->assertEquals(false, $consentCookie->arePurposesAllowed([]), 'Purposes should not be allowed');
 
-        $this->assertEquals(true, $consentCookie->arePurposesAllowed([1, 2, 3]), "Purposes should be allowed");
-        $this->assertEquals(true, $consentCookie->arePurposesAllowed([3, 1, 2]), "Purposes should be allowed");
+        $this->assertEquals(true, $consentCookie->arePurposesAllowed([1, 2, 3]), 'Purposes should be allowed');
+        $this->assertEquals(true, $consentCookie->arePurposesAllowed([3, 1, 2]), 'Purposes should be allowed');
 
         // Cookie with no allowed purposes
-        $consentCookie_2 = new ConsentCookie("BOXhscYOXhscYACABDENAEAAAAAAyADAALAAcACgAGA");
+        $consentCookie_2 = new ConsentCookie('BOXhscYOXhscYACABDENAEAAAAAAyADAALAAcACgAGA');
 
-        $this->assertEquals(false, $consentCookie_2->arePurposesAllowed([1, 2, 3]), "Purposes should not be allowed");
+        $this->assertEquals(false, $consentCookie_2->arePurposesAllowed([1, 2, 3]), 'Purposes should not be allowed');
     }
 
     public function test_isVendorAllowed()
     {
-        $consentCookie = new ConsentCookie("BOXhscYOXhscYACABDENAE4AAAAAyADAALAAcACgAGA");
+        $consentCookie = new ConsentCookie('BOXhscYOXhscYACABDENAE4AAAAAyADAALAAcACgAGA');
 
-        $this->assertEquals(false, $consentCookie->isVendorAllowed(1, [3, 1, 2]), "Vendor should not be allowed");
-        $this->assertEquals(false, $consentCookie->isVendorAllowed(5, [4, 1, 2]), "Vendor should not be allowed");
+        $this->assertEquals(false, $consentCookie->isVendorAllowed(1, [3, 1, 2]), 'Vendor should not be allowed');
+        $this->assertEquals(false, $consentCookie->isVendorAllowed(5, [4, 1, 2]), 'Vendor should not be allowed');
 
-        $this->assertEquals(true, $consentCookie->isVendorAllowed(5, [3, 1, 2]), "Vendor should be allowed");
-        $this->assertEquals(true, $consentCookie->isVendorAllowed(5), "Vendor should be allowed");
+        $this->assertEquals(true, $consentCookie->isVendorAllowed(5, [3, 1, 2]), 'Vendor should be allowed');
+        $this->assertEquals(true, $consentCookie->isVendorAllowed(5), 'Vendor should be allowed');
     }
 
     public function test_checkBinaryLength_base_data()
     {
         $this->expectException(InvalidArgumentException::class);
 
-        $consentCookie = new ConsentCookie("BOXiPiyOXiPiyAAABAENAAAAoAA");
+        $consentCookie = new ConsentCookie('BOXiPiyOXiPiyAAABAENAAAAoAA');
     }
 
     public function test_checkBinaryLength_bitfield_data()
     {
         $this->expectException(InvalidArgumentException::class);
 
-        $consentCookie = new ConsentCookie("BOXiPiyOXiPiyAAABAENAAAAAAAAoA");
+        $consentCookie = new ConsentCookie('BOXiPiyOXiPiyAAABAENAAAAAAAAoA');
     }
 
     public function test_values_from_setters()
@@ -188,36 +187,50 @@ class ConsentCookieTest extends TestCase
 
         $values =
         [
-            "Version"           => 1,
-            "Created"           => "2018-11-20 10:23:49.600000",
-            "LastUpdated"       => "2018-11-20 10:23:49.600000",
-            "CmpId"             => 2,
-            "CmpVersion"        => 1,
-            "ConsentScreen"     => 3,
-            "ConsentLanguage"   => "EN",
-            "VendorListVersion" => 4,
-            "PurposesAllowed"   => [
+            'Version'           => 1,
+            'Created'           => '2018-11-20 10:23:49.600000',
+            'LastUpdated'       => '2018-11-20 10:23:49.600000',
+            'CmpId'             => 2,
+            'CmpVersion'        => 1,
+            'ConsentScreen'     => 3,
+            'ConsentLanguage'   => 'EN',
+            'VendorListVersion' => 4,
+            'PurposesAllowed'   => [
                 1, 2, 3
             ],
-            "MaxVendorId"       => 12,
-            "EncodingType"      => 1,
-            "BitField"          => "001000010000",
-            "NumEntries"        => 1,
-            "DefaultConsent"    => true,
-            "RangeEntries"      => [
+            'MaxVendorId'       => 12,
+            'EncodingType'      => 1,
+            'BitField'          => '001000010000',
+            'NumEntries'        => 1,
+            'DefaultConsent'    => true,
+            'RangeEntries'      => [
                 [
-                    "singleOrRange"     => 0,
-                    "singleVendorId"    => 5,
+                    'singleOrRange'     => 0,
+                    'singleVendorId'    => 5,
                 ],
                 [
-                    "singleOrRange"     => 1,
-                    "startVendorId"     => 7,
-                    "endVendorId"       => 10,
+                    'singleOrRange'     => 0,
+                    'singleVendorId'    => 2,
                 ],
                 [
-                    "singleOrRange"     => 0,
-                    "singleVendorId"    => 12,
-                ]
+                    'singleOrRange'     => 1,
+                    'startVendorId'     => 7,
+                    'endVendorId'       => 10,
+                ],
+                [
+                    'singleOrRange'     => 1,
+                    'startVendorId'     => 4,
+                    'endVendorId'       => 3,
+                ],
+                [
+                    'singleOrRange'     => 0,
+                    'singleVendorId'    => 12,
+                ],
+                [
+                    'singleOrRange'     => 1,
+                    'startVendorId'     => 3,
+                    'endVendorId'       => 9,
+                ],
             ]
         ];
 
@@ -238,8 +251,8 @@ class ConsentCookieTest extends TestCase
 
     public function test_decodeWebSafeString()
     {
-        $consentCookie = new ConsentCookie("BOXHb99OXHb_BAOABBFRB2-AAAAid7_______9______9uz_Gv_v_f__33e8__9v_l_7_-___u_-33d4-_1vf99yfm1-7ftr3tp_86ues2_Xur_959__3z27EA");
+        $consentCookie = new ConsentCookie('BOXHb99OXHb_BAOABBFRB2-AAAAid7_______9______9uz_Gv_v_f__33e8__9v_l_7_-___u_-33d4-_1vf99yfm1-7ftr3tp_86ues2_Xur_959__3z27EA');
 
-        $this->assertEquals([1, 2, 3, 4, 5], $consentCookie->getPurposesAllowed(), "PurposesAllowed value not valid. Websafe decode failed.");
+        $this->assertEquals([1, 2, 3, 4, 5], $consentCookie->getPurposesAllowed(), 'PurposesAllowed value not valid. Websafe decode failed.');
     }
 }
